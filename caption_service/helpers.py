9335d29db483ea8c9801573f2868c29af34c6936
TarunGuha <tsguha44@gmail.com>
2023-05-21 21:09:56 +0530

    AWS

--- caption_service/helpers.py
+++ caption_service/helpers.py
@@ -34,6 +34,6 @@ def clean_caption(input_text, fillers):
 
 def download_model():
     wget.download(
-        "https://download1508.mediafire.com/voixmr76cs1ggnx_jesA210-zUO7IqsK8OUPMF8mEII9OorFBHIM6LwB0iziAuw0azRLBKRrTRbRdjnWY93hrdccJXgPiQNdoKWpjBJHARch8LWZNg9CTa0XnaDZuuiE6kpMcJMMhIi3W0P1BhamMyQiYQoRtGDLmjbI-E4RdTTdOXQ/7zjzmtodriha62c/model.pkl")
+        "https://image-caption-1.s3.ap-south-1.amazonaws.com/model.pkl")
     wget.download(
-        'https://drive.google.com/uc?export=view&id=1KJ50-zxUqiJlmdQLikBLpp3wa-a33bTR')
+        'https://image-caption-1.s3.ap-south-1.amazonaws.com/processor.pkl')
