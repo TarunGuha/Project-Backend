39111aa365955f73295adcc6b986f2c71ee9afce
TarunGuha <tsguha44@gmail.com>
2023-05-21 19:43:35 +0530

    Startup Change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Union,Dict
+from typing import Union, Dict
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI ,Query,Request,status
+from fastapi import FastAPI, Query, Request, status
 from pydantic import BaseModel
 from pydantic import BaseModel, validator
 from fastapi.encoders import jsonable_encoder
@@ -10,6 +10,7 @@ from fastapi.exceptions import HTTPException
 
 from caption_service.caption import initialize_caption
 from caption_service import caption
+from caption_service.helpers import download_model
 
 app = FastAPI()
 
@@ -20,17 +21,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class image_data(BaseModel):
-    image_url: Union[str,None] = None
+    image_url: Union[str, None] = None
+
 
 @app.on_event("startup")
 def startup():
+    download_model()
     initialize_caption()
+
 
 @app.get("/")
 def read_item():
     return "Hello World"
 
+
 @app.post("/get_caption")
-def get_caption(image_data:image_data):
+def get_caption(image_data: image_data):
     return caption.caption.caption_generator(image_data)
